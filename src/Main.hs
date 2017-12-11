591279b0f929b44bb8c6aaed41083d020d81a918
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2017-12-11 22:25:57 +0900

    Create a skeleton of the app

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,26 @@
+{-# LANGUAGE RecordWildCards #-}
 module Main where
 
+import Miso
+
+data Action
+    = NoOp
+
+type Color = Int
+type Field = [[Maybe Color]]
+
 main :: IO ()
-main = putStrLn "Hello, SameGame!"
+main = startApp App {..}
+    where
+        initialAction = NoOp
+        model         = undefined
+        update        = updateField
+        view          = viewField
+        subs          = []
+        events        = defaultEvents
+
+updateField :: Action -> Field -> Effect Action Field
+updateField = undefined
+
+viewField :: Field -> View Action
+viewField = undefined
