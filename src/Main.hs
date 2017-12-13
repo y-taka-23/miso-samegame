35ee4769cd64b57269ba2da8c9293c7165637270
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2017-12-13 14:58:30 +0900

    [WIP] Implement the logic to erase blocks

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE RecordWildCards #-}
 module Main where
 
+import Data.Maybe ( isJust )
 import Miso
 
 data Action
     = NoOp
+    | Knockout Position
 
 type Color = Int
 type Field = [[Maybe Color]]
+type Position = (Int, Int)
 
 main :: IO ()
 main = startApp App {..}
@@ -21,7 +24,14 @@ main = startApp App {..}
         mountPoint    = Nothing
 
 updateField :: Action -> Field -> Effect Action Field
-updateField = undefined
+updateField (Knowkout pos) = noEff . sweep . (mark pos)
+updateField NoOp           = noEff
+
+mark :: Position -> Field -> Field
+mark = undefined
+
+sweep :: Field -> Field
+sweep = filter (not . null) . map (filter isJust)
 
 viewField :: Field -> View Action
 viewField = undefined
